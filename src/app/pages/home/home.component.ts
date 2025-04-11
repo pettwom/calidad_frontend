575a381ccb30c07613155876031a6c666e47d659
Petter Meave Meracado <pettertwom@gmail.com>
2025-04-11 16:37:13 -0400

    ajustes dashboard

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -48,6 +48,74 @@ export class HomeComponent implements OnInit {
       processing: true,
       language: LanguageApp.spanish_datatables,
       searching: true,
+  //    fixedHeader: true,
+      dom: 'Bfrtip', // Para incluir los botones
+      buttons: [
+        {
+          extend: 'print',
+          text: '<i class="fa-solid fa-print"></i> Imprimir',
+          className: 'btn btn-warning',
+        },
+
+        {
+          extend: 'excel',
+          text: '<i class="fa-solid fa-file-excel"></i> Descargar Excel',
+          title: 'Reporte de Alertas',
+          className: 'btn btn-info',
+          exportOptions: {
+            // Especificar las columnas que quieres exportar por índice
+            columns: [0, 1, 2, 3, 4, 5, 6, 7,8,9,10], // Solo exportar las columnas "Nombre" (índice 0) y "Edad" (índice 1)
+          },
+          customize: function (xlsx) {
+            let sheet = xlsx.xl.worksheets['sheet1.xml'];
+            // Modificar el fondo de los encabezados
+            const headers = sheet
+              .getElementsByTagName('row')[0]
+              .getElementsByTagName('c');
+            for (let i = 0; i < headers.length; i++) {
+              const header = headers[i];
+              // Agregar fondo color gris claro a los encabezados
+              header.setAttribute('s', '30'); // 's' hace referencia al estilo en Excel (fondo de celda)
+            }
+
+            // Cambiar el tamaño de las fuentes y poner en negrita los encabezados
+            const rows = sheet.getElementsByTagName('row');
+            for (let row of rows) {
+              const cells = row.getElementsByTagName('c');
+              for (let cell of cells) {
+                const style = cell.getAttribute('s');
+                if (style && style === '30') {
+                  // Si el estilo es de los encabezados, hacemos que el texto sea negrita
+                  cell.setAttribute('t', 'inlineStr');
+                  const istring = document.createElement('is');
+                  const t = document.createElement('t');
+                  t.textContent = cell.textContent; // el valor del título de la celda
+                  istring.appendChild(t);
+                  cell.textContent = '';
+                  cell.appendChild(istring);
+                  // Aplicamos estilo de texto negrita a los encabezados
+                  cell.setAttribute('s', '2'); // '2' es el estilo en Excel para texto en negrita
+                }
+              }
+            }
+            // Comprobar si ya existe el filtro y agregarlo si no está presente
+            // Comprobar si ya existe el filtro y agregarlo si no está presente
+            const autofilter = sheet.getElementsByTagName('autoFilter');
+            if (autofilter.length === 0) {
+              const autoFilter = document.createElement('autoFilter');
+              autoFilter.setAttribute('ref', 'A2:K2'); // Definir el rango de columnas para el filtro
+
+              // Asegúrate de insertarlo en la estructura correcta del XML (dentro de <worksheet>).
+              let worksheetNode = sheet.getElementsByTagName('worksheet')[0];
+
+              // Insertamos el filtro en el lugar correcto
+              if (worksheetNode) {
+                worksheetNode.appendChild(autoFilter);
+              }
+            }
+          },
+        },
+      ],
     };
     this.serviceService.get(`/home/getEstadisticas`).subscribe((res: any) => {
       this.datosEst = res.text[0];
@@ -78,7 +146,7 @@ this.getTransferidos();
     this.serviceService.get(`/home/getAprobados`).subscribe((res: any) => {
       this.aprobadosList = res.data;
       console.log(this.aprobadosList);
-      
+
     });
   }
   getObservados() {
@@ -93,7 +161,7 @@ this.getTransferidos();
   }
   divModal(tipo) {
     console.log(tipo);
-    
+
     switch (tipo) {
       case 'aprobado':
         this.alertasAprobdos = true;
